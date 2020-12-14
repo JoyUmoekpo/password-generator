caa0f23048e6edfc33b93478f85dbfc5dd9b5335
Joy <joyumoekpo1@gmail.com>
2020-12-14 15:10:24 -0600

    combined character variables

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ var generateBtn = document.querySelector("#generate");
 var hello = "Hello user."
 var passLength = "How long would you like your password?"
 
-var passMin = 8
-var passMax = 128
-var num = "1234567890"
-var charLow = "abcdefghijklmnopqrstuvwxyz"
-var charHigh = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var specChar = "!@#$%^&*()_-+<>?;:'{}[]"
+alert(hello)
+prompt(passLength)
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword() {
+    var  randChar= '1234567890' + 'abcdefghijklmnopqrstuvwxyz' + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + '!@#$%^&*()_-+<>?;:[]',
 
-  passwordText.value = password;
+  for ( i = 1, i = 8, i = 128; i++;) {
+    var randChar = Math.floor(Math.random() * numb.length + 1)
+  }
+
+}
+
+var passwordText = document.querySelector("#password");
+passwordText.value = password;
 
 }
 
 generateBtn.addEventListener("click", writePassword);
-
-alert(hello)
-Number(prompt(passLength))
