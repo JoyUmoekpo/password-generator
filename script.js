7341a0b8e1160c54800be02e1f92b7ca1222ad71
Joy <joyumoekpo1@gmail.com>
2020-12-14 11:33:56 -0600

    added alert and prompt message

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var hello = "Hello user."
+var passLength = "How long would you like your password?"
 
 // Write password to the #password input
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,3 +15,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+alert(hello)
+prompt(passLength)
+
