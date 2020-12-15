005914191fef575d57ae2551fed0500667496d8d
Joy <joyumoekpo1@gmail.com>
2020-12-14 19:57:50 -0600

    modified functions and variables

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
-var numbs = '1234567890' 
-var specs = './!@#$%^&*' 
-var lowers = 'abcdefghijklmnopqrstuvwxyz' 
-var uppers = 'ABCDEFGHIJKLMNOPQRSTUWXYZ'
-
 var password = "";
-var insertUser = "";
 
-function generatePassword() {
+var generatePassword = function() {
   password = "";
-  var passLength = insertNumUser();
-  var chars = insertCharUser();
-}
+  var passwordLeng = insertNumUser();
 
-function insertNumUser() {
-  var passLength = prompt("How long would you like your password? (between 8 and 128 characters)")
+  var passwordChar = insertCharUser();
+  var passCharLength = passwordChar.length;
 
-  if (passLength < 8 || passLength > 128 || passLength === "") {
-    alert("Password can only be between 8 and 128 characters in length.")
+  for (var i = 0; i < passwordLeng; i++) {
+    password += passwordChar.charAt(Math.floor(Math.random() * passCharLength));
   }
 
-  passLength = Math.floor(Number(passLength))
+  return password;
+};
 
+var insertNumUser = function() {
+  var passLength = prompt("How long would you like your password? (between 8 and 128 characters)");
+
+  if (passLength < 8 || passLength > 128 || passLength === "") {
+    alert("Password can only be between 8 and 128 characters in length.");
+    return insertNumUser;
+  }
+
+  passLength = Math.floor(Number(passLength));
+
+  return passLength;
 }
 
+var insertCharUser = function() {
+  var passwordChar = "";
 
-function insertCharUser() {
-  
+  var numbs = "1234567890";
+  var specs = "./!@#$%^&*";
+  var uppers = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
+  var lowers = "abcdefghijklmnopqrstuvwxyz";
+
+
+  var number = confirm("Do you want numbers in your password?");
+  var special = confirm("Do you want special characters in your password?");
+  var upperCase = confirm("Do you want uppercase letters in your password?");
+  var lowerCase = confirm("Do you want lowercase letters in your password?");
+
+  if (number == true) {
+    passwordChar += numbs;
+  }
+  if (special == true) {
+    passwordChar += specs;
+  }
+  if (upperCase == true) {
+    passwordChar += uppers;
+  }
+  if (lowerCase == true) {
+    passwordChar += lowers;
+  }
+
+  return passwordChar;
+
 }
 
 var generateBtn = document.querySelector("#generate");
